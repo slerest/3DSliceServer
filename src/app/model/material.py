e592f98c4f5277c413c91fb98c12ffd0868d6678
SLeRest <sebastienlerest22@gmail.com>
2021-09-05 14:35:06 +0200

    start material

---
+++ src/app/model/material.py
@@ -0,0 +1,74 @@
+from model.base import BaseModel
+from schema.material import MaterialOut
+from sqlalchemy import (
+    Column,
+    Integer,
+    Float,
+    String,
+    Boolean,
+    ForeignKey
+)
+
+class Material(BaseModel):
+    __tablename__ = 'MATERIAL'
+
+    supplier = Column('SUPPLIER', String, nullable=False)
+    name = Column('NAME', String, nullable=False)
+    general_type = Column('GENERAL_TYPE', String, nullable=False)
+    specific_type = Column('SPECIFIC_TYPE', String, nullable=False)
+    am_process = Column('AM_PROCESS', String, nullable=False)
+    post_process = Column('AM_PROCESS', Boolean, default=None, nullable=True)
+    ultimate_temsile_strength_min = Column('ULTIMATE_TENSILE_STRENGTH_MIN', Float, default=None, nullable=True)
+    ultimate_temsile_strength_max = Column('ULTIMATE_TENSILE_STRENGTH_MAX', Float, default=None, nullable=True)
+    tensible_modulus_min = Column('TENSILE_MODULUS_MIN', Float, default=None, nullable=True)
+    tensible_modulus_max = Column('TENSILE_MODULUS_MAX', Float, default=None, nullable=True)
+    elongation_at_break_min = Column('ELONGATION_AT_BREAK_MIN', Float, default=None, nullable=True)
+    elongation_at_break_max = Column('ELONGATION_AT_BREAK_MAX', Float, default=None, nullable=True)
+    flexural_strength_min = Column('FLEXURAL_STRENGTH_MIN', Float, default=None, nullable=True)
+    flexural_strength_max = Column('FLEXURAL_STRENGTH_MAX', Float, default=None, nullable=True)
+    flexural_modulus_min = Column('FLEXURAL_MODULUS_MIN', Float, default=None, nullable=True)
+    flexural_modulus_max = Column('FLEXURAL_MODULUS_MAX', Float, default=None, nullable=True)
+    hardness_shore_scale = Column('HARDNESS_SHORE_SCALE', String, default=None, nullable=True)
+    hardness_min = Column('HARDNESS_MIN', Float, default=None, nullable=True)
+    hardness_max = Column('HARDNESS_MAX', Float, default=None, nullable=True)
+    hdt_min = Column('HDT_MIN', Float, default=None, nullable=True)
+    hdt_max = Column('HDT_MAX', Float, default=None, nullable=True)
+    glass_transition_temp_min = Column('GLASS_TRANSITION_TEMP_MIN', Float, default=None, nullable=True)
+    glass_transition_temp_max = Column('GLASS_TRANSITION_TEMP_MAX', Float, default=None, nullable=True)
+    part_density = Column('PART_DENSITY', Float, default=None, nullable=True)
+    flammability = Column('FLAMMABILITY', String, default=None, nullable=True)
+    usp_class_vi_certified = Column('USP_CLASS_VI_CERTIFIED', Boolean, default=None, nullable=True)
+    availability = Column('AVAILABILITY', Boolean, default=None, nullable=True)
+
+    def ToMaterialOut(self) -> MaterialOut:
+        return MaterialOut(
+            supplier = self.supplier,
+            name = self.name,
+            general_type = self.general_type,
+            specific_type = self.specific_type,
+            am_process = self.am_process,
+            post_process = self.post_process,
+            ultimate_temsile_strength_min = self.ultimate_temsile_strength_min,
+            ultimate_temsile_strength_max = self.ultimate_temsile_strength_max,
+            tensible_modulus_min = self.tensible_modulus_min,
+            tensible_modulus_max = self.tensible_modulus_max,
+            elongation_at_break_min = self.elongation_at_break_min,
+            elongation_at_break_max = self.elongation_at_break_max,
+            flexural_strength_min = self.flexural_strength_min,
+            flexural_strength_max = self.flexural_strength_max,
+            flexural_modulus_min = self.flexural_modulus_min,
+            flexural_modulus_max = self.flexural_modulus_max,
+            hardness_shore_scale = self.hardness_shore_scale,
+            hardness_min = self.hardness_min,
+            hardness_max = self.hardness_max,
+            hdt_min = self.hdt_min,
+            hdt_max = self.hdt_max,
+            glass_transition_temp_min = self.glass_transition_temp_min,
+            glass_transition_temp_max = self.glass_transition_temp_max,
+            part_density = self.part_density,
+            flammability = self.flammability,
+            usp_class_vi_certified = self.usp_class_vi_certified,
+            availability = self.availability,
+            created_at = self.created_at,
+            updated_at = self.updated_at
+        )
--- src/app/schema/material.py
+++ src/app/schema/material.py
@@ -9,25 +9,25 @@ class MaterialOut(DateTimeModelMixin, IDModelMixin):
     general_type: str
     specific_type: str
     am_process: str
-    post_process: bool
-    ultimate_tensible_strength_min: float
-    ultimate_tensible_strength_max: float
-    tensile_modulus_min: float
-    tensile_modulus_max: float
-    elongation_at_break_min: float
-    elongation_at_break_max: float
-    flexural_strength_min: float
-    flexural_strength_max: float
-    flexural_modulus_min: float
-    flexural_modulus_max: float
-    hardness_shore_scale: str
-    hardness_min: float
-    hardness_max: float
-    hdt_min: float
-    hdt_max: float
-    glass_transition_temp_min: float
-    glass_transition_temp_max: float
-    part_density: float
-    flammability: str
-    usp_class_vi_certified: bool
-    availability: bool
+    post_process: Optional[bool]
+    ultimate_tensible_strength_min: Optional[float]
+    ultimate_tensible_strength_max: Optional[float]
+    tensile_modulus_min: Optional[float]
+    tensile_modulus_max: Optional[float]
+    elongation_at_break_min: Optional[float]
+    elongation_at_break_max: Optional[float]
+    flexural_strength_min: Optional[float]
+    flexural_strength_max: Optional[float]
+    flexural_modulus_min: Optional[float]
+    flexural_modulus_max: Optional[float]
+    hardness_shore_scale: Optional[str]
+    hardness_min: Optional[float]
+    hardness_max: Optional[float]
+    hdt_min: Optional[float]
+    hdt_max: Optional[float]
+    glass_transition_temp_min: Optional[float]
+    glass_transition_temp_max: Optional[float]
+    part_density: Optional[float]
+    flammability: Optional[str]
+    usp_class_vi_certified: Optional[bool]
+    availability: Optional[bool]
